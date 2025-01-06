4624c87d01cbc974e3dd8259933ca55fd2e8eb60
NURIA-LUCIA PLOSCA <nuria.plosca@stud.ubbcluj.ro>
2025-01-06 19:42:18 +0200

    filtrare

--- Pages/MenuItems/Index.cshtml.cs
+++ Pages/MenuItems/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Menuzama1.Data;
 using Menuzama1.Models;
@@ -19,14 +20,32 @@ namespace Menuzama1.Pages.MenuItems
             _context = context;
         }
 
-        public IList<MenuItem> MenuItem { get;set; } = default!;
+        public IList<MenuItem> MenuItem { get; set; } = default!;
+        public SelectList Categories { get; set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string SelectedCategory { get; set; }
 
         public async Task OnGetAsync()
         {
-            MenuItem = await _context.MenuItem
-                 .Include(m => m.Category)
+            // Obține toate categoriile pentru dropdown
+            var categoriesQuery = _context.Category.Select(c => c.Name);
+
+            // Creează interogarea pentru articolele din meniu
+            var menuItems = _context.MenuItem
+                .Include(m => m.Category)
                 .Include(m => m.MenuItemType)
-                .ToListAsync();
+                .AsQueryable(); // Transformă în IQueryable
+
+            // Aplică filtrarea dacă există o categorie selectată
+            if (!string.IsNullOrEmpty(SelectedCategory))
+            {
+                menuItems = menuItems.Where(m => m.Category.Name == SelectedCategory);
+            }
+
+            // Populează modelul
+            Categories = new SelectList(await categoriesQuery.Distinct().ToListAsync());
+            MenuItem = await menuItems.ToListAsync();
         }
+
     }
 }
